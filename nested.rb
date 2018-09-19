9f237354e7ceae3d4e28b3d9d705cca783eb400d
Stef Claus <stef.claus@gmail.com>
2018-09-19 02:35:43 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -138,7 +138,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:known_for]
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
   
 end
